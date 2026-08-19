58b1e9e447a9af6a9bac30d1f14ea99ee9994f4f
Salimtech353 <techbark353@gmail.com>
2026-08-19 09:57:05 +0600

    Upgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
   namespace = "com.aistudio.slmweather.kxqvtr"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.aistudio.slmweather.kxqvtr"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
